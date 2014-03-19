412bc4ac15697f505eb927d35bf4ea7654be953c
quetzalcoatl41 <d-heartless@hotmail.com>
2014-03-19 16:35:03 -0400

    el login

---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html lang="en" class="ie6 ielt8"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="ie7 ielt8"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="ie8"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--> <html lang="en"> <!--<![endif]-->
+<head>
+		<link rel="stylesheet" href="stylelogin.css" type="text/css">		
+</head>
+<body>
+<div class="container">
+	<section id="content">
+		<form action="registro_correcto.php" method='POST'>
+			<h1>Login Form</h1>
+			<div>
+				<input type="text" placeholder="Username" required="" id="user" />
+			</div>
+			<div>
+				<input type="password" placeholder="Password" required="" id="pass" />
+			</div>			     
+			<div>
+				<input type="submit" value="Log in" />
+				<a href="#">Lost your password?</a>
+				<a href="#">Register</a>
+			</div>
+		</form><!-- form -->
+	</section><!-- content -->
+</div><!-- container -->
+</body>
+</html>
