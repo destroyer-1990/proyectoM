d87cbfb45d12d97a7cbead8677a77ad93de3813f
destroyer-1990 <octaviodomsal@hotmail.com>
2014-03-19 17:08:48 -0400

    ya quedo

--- login.php
+++ login.php
@@ -1,10 +1,6 @@
-<!DOCTYPE html>
-<!--[if lt IE 7 ]> <html lang="en" class="ie6 ielt8"> <![endif]-->
-<!--[if IE 7 ]>    <html lang="en" class="ie7 ielt8"> <![endif]-->
-<!--[if IE 8 ]>    <html lang="en" class="ie8"> <![endif]-->
-<!--[if (gte IE 9)|!(IE)]><!--> <html lang="en"> <!--<![endif]-->
+<html>
 <head>
-		<link rel="stylesheet" href="stylelogin.css" type="text/css">		
+	<link rel="stylesheet" href="stylelogin.css" type="text/css">		
 </head>
 <body>
 <div class="container">
