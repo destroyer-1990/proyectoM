072cc92a1a66060fac71b37cdf0cac38b2376e09
chelo556 <marcelobarrera556@gmail.com>
2014-03-21 02:08:36 -0600

    Tratando de Agregar un poco de seguridad

--- conDrupal/comentar.php
+++ conDrupal/comentar.php
@@ -1,13 +1,19 @@
 <?php
-	if (empty($_POST["idNode"])||empty($_POST["comentario"]))
-	return;
-	
-	require_once("cNodo.php");
-	echo 'hola';
+	$aux = require_once("cNodo.php");
+
+	//No se pudo importar el archivo cNodo.php
+	if (!$aux){
+		header('Location: http://debian7proj.cloudapp.net/');
+	}
+
+	//variables post vacias y un poco de validación de datos en cuanto a idNode
+	if ( $_POST["idNode"] < 10 ||$_POST["idNode"] > 0 || !is_int($_POST["idNode"]) || empty($_POST["idNode"]) || empty($_POST["comentario"]))
+	header('Location: http://debian7proj.cloudapp.net/');
 	
 	$obj = new nodo();
 	$obj->comentar($_POST["idNode"],$_POST["comentario"]);
 
+	//Regresar usuario a la página de donde teóricamente llegó
 	header('Location: http://debian7proj.cloudapp.net/articulo.php?idNode='.$_POST['idNode']);
 
 ?>
