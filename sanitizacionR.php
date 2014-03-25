5009dec897cfdefa3b45cefb67a6038ca745c906
quetzalcoatl41 <d-heartless@hotmail.com>
2014-03-25 13:24:49 -0400

    Registro a Base de datos

--- sanitizacionR.php
+++ sanitizacionR.php
@@ -1,4 +1,21 @@
 <?php
+
+ class MyDB extends SQLite3
+          {
+                function __construct()
+	                    {
+	                           $this->open('bdfrontend.db');
+		                                 }
+		                                }
+			    
+			     $db = new MyDB();
+				     if(!$db){
+				            echo $db->lastErrorMsg();
+				               } else {
+				                      echo "Opened database successfully\n";
+					                    }
+
+
 $user=$_POST["usuario"];
 $pass=$_POST["password"];
 $conpass=$_POST["confirmar_password"];
@@ -17,5 +34,56 @@ echo "<html><body><h1>fallo el uuario</h1></body></html>";}
 else{
 	header("Location: registro.php");
 }
+//$anterior = $nuevo + 1;
+//$nuevo=$anterior+1;
+$results = $db->query('select max(idusuario) from usuarios');
+//print $results;
+while($row = $results->fetchArray()){
+var_dump($row);
+//$rows = $consulta->numRows();
+
+//echo "Number of rows:echo Number of rows: $rows";
+}
+
+//$lastid = $db->lastInsertRowid();
+//echo "valor $lastid";
+/*$consulta = sqlite_query($db, "SELECT * FROM usuarios");
+$rows = sqlite_num_rows($consulta);
+print "Numero de registros: $rows";
+*/
+
+//$consulta = $db->query("SELECT * FROM usuarios");
+//$rows = $consulta->numRows();
+
+//echo "Number of rows:echo Number of rows: $rows";
+
+
+//$indice = 1;
+
+
+$hashpass = md5($pass);
+
+ $lastid = $db->lastInsertRowid();
+    echo "valor antes $lastid";
+
+
+
+$sql =<<<EOF
+      INSERT INTO usuarios (idusuario,login,password,orden)
+	  VALUES(13,'$user','$hashpass','ABCDEFGH');
+EOF;
+
+			 $ret = $db->exec($sql);
+			     if(!$ret){
+				           echo $db->lastErrorMsg();
+						                } else {
+										                   echo "Records created successfully\n";
+														                         }   
+$lastid = $db->sqlite_num_rows();
+ echo "valor despues $lastid";
+
+		$db->close();
+
+
 ?>
 
