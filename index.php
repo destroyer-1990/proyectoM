200cb7ab1cf0c292034d059d1cbe5d4d08d4baa8
destroyer-1990 <octaviodomsal@hotmail.com>
2014-03-30 15:13:11 -0400

    ya esta terminado

--- index.php
+++ index.php
@@ -9,25 +9,71 @@
 		<script type="text/javascript" src="scripts/posicion.php"></script>
      	</head>
 	<body>        
-	<?php if($u) echo '<h2>Bienvenido: '.$_SESSION['user'].'</h2>'."\n";?>
+	<?php if($u) echo '<h2>Bienvenido: '.$_SESSION['user'].'</h2>'."\n";
+	/*Aqui va la parte de la base*/
+
+	class MyDB extends SQLite3
+	            {
+	                     function __construct(){
+								   $this->open('scripts/base2.db');
+			                                          }
+			                                          }
+				  
+				                   $db = new MyDB();
+				                         if(!$db){
+				                                echo $db->lastErrorMsg();
+					                           } else {
+                                      //echo "Opened database successfully\n";
+					                                         }
+
+//$usuario=$_SESSION['user'];
+echo $_SESSION['user'];
+//$usuario = "duby";	
+$usuario = $_SESSION['user'];
+
+$sql =<<<EOF
+SELECT orden from usuarios where login='$usuario';
+EOF;
+
+
+$ret = $db->query($sql);
+              while($row = $ret->fetchArray(SQLITE3_ASSOC) ){                   //echo "orden = ". $row['orden'] . "\n";
+   
+$orden = $row['orden'];
+//print "Variable extraida:$orden\n";
+   
+}
+
+
+
+	/*****************/
+	
+	?>
 		<div id="cabecera" align= "right" >
 		<form action="login.php" method='POST'>
 		<input id="login" type="submit" value="login">
 		</form>
-	<?php if ($u){  ?>
-		<form action="login.php" method='POST'>
-                <input id="guardar" type="submit" value=posicion>
-		</form><?php } ?>
+	<?php if ($u){
+		echo '<a href=index.php><input id="guardar" type="submit" value="Guardar" style="cursor:pointer;width:55px;"></a>';
+		} ?>
+	<?php if ($u && $usuario == 'admin'){
+		echo '<a href=registro.php><input id="RegistrarU" type="submit" value="RegistrarU" style="cursor:pointer;width:55px;"></a>';
+		} ?>
 	<?php if ($u){echo '<a href="cierre.php"><h2>Cierre de sesion</h2></a> '."\n";} ?>
 		</div>
         <div class="proy">
 		<ul id="sortable">
 			<?php
 			/*$array =  array(1 => 1,2 => 2,3 => 4,4 => 7,5 => 8,6 => 3,7 => 5,8 => 6,9 => 9);*/
-			$array = array(1,2,3,4,5,6,7,8,9);
+			//$string = "213456789";
+			$string = $orden;
+			//print "Variable extraida:$orden\n";
+	        //$array = array(2,1,3,4,5,6,7,8,9);
+			$array = str_split($string);
 			foreach($array as $num):?>
 			<li id="draggable<?=$num?>" ><a href="articulo.php?idNode=<?=$num?>"><img src="css/imagenes/pokebola.png" alt="" width="250" height="200"><small>casilla numero <?=$num?></small></a></li>
-			<?php endforeach; ?>
+			<?php endforeach;
+			$db->close(); ?>
 		</ul>
 		</div>
 	</body>
